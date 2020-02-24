51df2f38dcbf3d8c16d3a2c5a30fd147c4cc6732
Aravind Chintalapudi <aravindchintalapudi1910@gmail.com>
2020-02-24 18:42:03 +0530

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,13 @@
+function validate()
+{
+    var name = document.form.user.value;
+    var pass = document.form.password.value;
+    if( name == "aravind_nemo" && pass == "Nemo1910!" )
+    {
+        alert("Login Successful");
+    }
+}
+function validater()
+{
+    alert("Enter a valid Email address. Ex: abc@xyz.com");
+}
